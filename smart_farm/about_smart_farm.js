0bd095e5d70a67c9dec226942996755f981a494d
shreenivasulu <shreenivasuluvallipi>
2026-08-19 14:22:21 +0530

    Updated smart farm

--- smart_farm/about_smart_farm.js
+++
@@ -1,5 +0,0 @@
-// Back To Top
-    document.querySelector(".back-top a").onclick = (backtoTop) => {
-        backtoTop.preventDefault();
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    };
--- smart_farm/product_smart_farm.js
+++
@@ -1,7 +0,0 @@
-const backTop = document.querySelector(".back-top a");
-if (backTop) {
-    backTop.addEventListener("click", (e) => {
-        e.preventDefault();
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
-}
